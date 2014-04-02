7b29cfaa25f968ad032ff96eb51697c5a94af92a
Paulino Mari� <paulinodjm@hotmail.com>
2014-04-02 19:53:04 +0200

    Add some scripts

---
+++ Assets/Item.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Renderer))]
+[RequireComponent(typeof(Timer))]
+public class Item : MonoBehaviour
+{
+    public int Weight = 1;
+
+    private Timer _timer;
+
+    protected void Start()
+    {
+        _timer = GetComponent<Timer>();
+        _timer.Triggered += OnTimerEnded;
+    }
+
+    protected void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.tag.Contains("Player")) return;
+
+        other.gameObject.GetComponent<PlayerController>().Score += Weight;
+        _timer.enabled = true;
+        SetPickable(false);
+    }
+
+    private void OnTimerEnded()
+    {
+        SetPickable(true);
+    }
+
+    private void SetPickable(bool enabled)
+    {
+        renderer.enabled = enabled;
+        collider.enabled = enabled;
+    }
+}
---
+++ Assets/Timer.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+/// <summary>
+/// Base class for each timer
+/// </summary>
+public class Timer : MonoBehaviour 
+{
+    /// <summary>
+    /// Timer duration
+    /// </summary>
+    public float TimerDuration;
+
+    private float _remainingTime;
+    public float RemainingTime
+    {
+        get
+        {
+            return (_remainingTime < .0f) ? .0f : _remainingTime;
+        }
+    }
+
+    /// <summary>
+    /// Event triggered when 
+    /// </summary>
+    public event Action Triggered;
+
+    #region Unity callback
+
+    protected void OnEnable()
+    {
+        _remainingTime = TimerDuration;
+    }
+
+    protected void Update()
+    {
+        if (!enabled) return;
+
+        _remainingTime -= Time.deltaTime;
+        if (_remainingTime <= .0f)
+        {
+            if (Triggered != null)
+            {
+                Triggered();
+            }
+            enabled = false;
+        }
+    }
+
+    #endregion
+}
